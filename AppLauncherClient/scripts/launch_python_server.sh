f94686d01f3c2d1221715d3950435b2c81f6a99c
wdlindmeier <wdlindmeier@gmail.com>
2013-11-23 09:54:56 -0500

    Tweaked python script

--- AppLauncherClient/scripts/launch_python_server.sh
+++ AppLauncherClient/scripts/launch_python_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-/bin/sh $DIR/launch_mpe_server.sh "/Users/bill/Tools/cinder_master/blocks/Most-Pixels-Ever-Cinder/mpe-python-server/mpe_server.py"
+/bin/sh $DIR/launch_mpe_server.sh "/Users/bill/Tools/cinder_master/blocks/Most-Pixels-Ever-Cinder/mpe-python-server/mpe_server.py --screens 3"
